8c7c4e771aa58c5951a4b36419e172c60b3cd148
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-05-15 01:13:15 +0530

    stuff module updated

--- mod.js
+++ mod.js
@@ -1,5 +1,5 @@
 var stuff = require('./stuff');
 
 console.log(stuff.counter(['Dumi','Chamal','sda','fgs']));
-console.log(stuff.pi);
+console.log(stuff.adder(stuff.pi,5));
 console.log(stuff.adder(3,6));

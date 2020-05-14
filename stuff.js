fc2dfa6fe7fa96b63eba72e73d1b65201a7a4e6c
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-05-15 01:19:10 +0530

    stuff module updated

--- stuff.js
+++ stuff.js
@@ -1,10 +1,25 @@
-module.exports.counter = function(arr){
+var counter = function(arr){
     return 'There are '+ arr.length + ' elements in this array!';
 };
 
-module.exports.adder = function(a,b){
+var adder = function(a,b){
     return `The sum of the 2 numbers is ${a+b}`; //used template string with ``. so no need to use concatinating
 };
 
-module.exports.pi = 3.142;
+var deduction = function(a,b){
+    return `The deduction of the 2 numbers is ${a-b}`; //used template string with ``. so no need to use concatinating
+};
 
+var pi = 3.142;
+
+
+
+// module.exports.pi = pi;
+
+module.exports = {
+    adder: adder,
+    deduction: deduction,
+    pi: pi,
+    counter: counter,
+
+};

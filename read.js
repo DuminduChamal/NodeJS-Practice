4ee95877d99008ee5231bf3b8a8406ef85cd08b8
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-05-16 03:02:55 +0530

    create and remove directory

--- read.js
+++ read.js
@@ -16,4 +16,23 @@ fs.readFile('readMe.txt','utf8', function(err,data){
 });
 console.log('Testing....');
 
+// fs.unlink() - to delete a file
+
+//sysncronize create and delete directory
+// fs.mkdirSync('Stuff');
+// fs.rmdirSync('Stuff');
+
+//Asyncronize
+fs.mkdir('Stuff',function(){
+    fs.readFile('readme.txt','utf8', function(err,data){
+        fs.writeFile('./Stuff/writeMeASYNC.txt', data, function(err, result) {
+            if(err) console.log('error', err);
+        });
+    });
+});
+fs.unlink('./Stuff/writeMeASYNC.txt', function(){
+    fs.rmdir('Stuff', function(){
+        console.log('Directory deleted!');
+    });
+});
 

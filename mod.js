d7700bf6366d36edb7cadcbc40afa4ece4123ae5
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-05-16 02:20:35 +0530

    custom events on objects

--- mod.js
+++ mod.js
@@ -3,3 +3,37 @@ var stuff = require('./stuff');
 console.log(stuff.counter(['Dumi','Chamal','sda','fgs']));
 console.log(stuff.adder(stuff.pi,5));
 console.log(stuff.adder(3,6));
+
+
+var events = require('events');
+
+var myEmitter = new events.EventEmitter();
+myEmitter.on('someEvent', function(msg){
+    console.log(msg);
+});
+
+myEmitter.emit('someEvent','The event was emmited!');
+
+//custom events on made up own objects
+var util = require('util');
+
+var Person = function(name){
+    this.name= name;
+};
+
+util.inherits(Person, events.EventEmitter);
+
+var James = new Person('James');
+var Ryan = new Person('Ryan');
+var Bobby = new Person('Bobby');
+
+var people = [James, Ryan, Bobby];
+
+people.forEach(function(person){
+    person.on('speak', function(msg){
+        console.log(person.name+' said: '+msg);
+    });
+});
+
+James.emit('speak','Hey!!!');
+Bobby.emit('speak', 'Worksssss!');
--- stuff.js
+++ stuff.js
@@ -21,5 +21,4 @@ module.exports = {
     deduction: deduction,
     pi: pi,
     counter: counter,
-
 };

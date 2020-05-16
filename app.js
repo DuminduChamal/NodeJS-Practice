97c465b57e05023e725562164c9d9317ce681b68
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-05-16 11:07:46 +0530

    second commit

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ console.log("Testing!!");
 setTimeout(function(){
     console.log("3 Seconds have passed");
 },3000);
+
+var time=0;
+setInterval(function(){
+    time+=2;
+    console.log(time+" Seconds have passed");
+},2000);

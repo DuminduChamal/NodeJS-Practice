a0594b27964118aa60170366407d5f4110625761
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-05-16 11:10:58 +0530

    second commit

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ setTimeout(function(){
 },3000);
 
 var time=0;
-setInterval(function(){
+// setInterval(function(){
+//     time+=2;
+//     console.log(time+" Seconds have passed");
+// },2000);
+
+var timer=setInterval(function(){
     time+=2;
     console.log(time+" Seconds have passed");
+    if(time>5)
+        clearInterval(timer);
 },2000);
+
